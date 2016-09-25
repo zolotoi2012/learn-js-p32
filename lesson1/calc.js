8746fd22a9477bbf73fdb81e537fee5b1ffb3f00
zolotoi2012 <ghost.krut@gmail.com>
2016-09-25 18:34:00 +0300

    Update calc.js

--- lesson1/calc.js
+++ lesson1/calc.js
@@ -7,35 +7,71 @@ var values = [16, 2135657879, 2222, 123, 434, 123, 77];
 var operators = ["+", "+", "*", "/", "-", "+"];
 var result = 0;
 
-/**
- ======================================
- YOUR CODE
- ======================================
- */
-
-for (var i = 0; i < operators.length; i++) {
-
-	switch(operators[i]) {
-		case "*":
-
-			break;
-		case "/":
-			
-			break;
-	}
-
+for (var i = 0; i < operators.length; i++) 
+{
+    switch(operators[i])
+        {
+            case '*':
+            {
+                value[i+1]=multiplication(value[i],value[i+1]);
+                value[i]=0;
+                if(i>0)operators[i]=operators[i-1];
+                break;  
+            };
+            case '/':
+            {
+                value[i+1]=division(value[i],value[i+1]);
+                value[i]=0;
+                if(i>0)operators[i]=operators[i-1];
+                break;  
+            };
+        }   
 }
 
-var sum = function (a, b) {
+for (var i = 0; i < operators.length; i++) 
+{
+    switch(operators[i])
+        {
+            case '+':
+            {
+                value[i+1]=addition(value[i],value[i+1]);
+                value[i]=0;
+                break;  
+            };
+            case '-':
+            {
+                value[i+1]=subtracting(values[i],values[i+1]);
+                value[i]=0;
+                break;  
+            };
+        }   
+}
 
-};
+function addition(a, b)
+{
+    a+=b;
+    return a;
+}
 
-var minus = function (a, b) {
+function subtracting(a, b)
+{
+    a-=b;
+    return a;
+}
 
-};
+function multiplication(a, b)
+{
+    a*=b;
+    return a;
+}
+
+function division(a, b)
+{
+    a/=b;
+    return a;
+}
 
 
 
-
-
+result=value[value.length-1]
 console.log(result);
